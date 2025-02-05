0a0a5c28474b66e452e8c0a57705edcc1b14e851
Smileycorp <callum.j.longbottom@gmail.com>
2025-02-05 23:53:26 +0000

    Update AffixGilded.java

--- src/main/java/net/smileycorp/elites/common/affixes/AffixGilded.java
+++ src/main/java/net/smileycorp/elites/common/affixes/AffixGilded.java
@@ -1,13 +1,25 @@
 package net.smileycorp.elites.common.affixes;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 
 public class AffixGilded extends Affix {
     
     @Override
-    public void dealDamage(LivingEntity entity, LivingEntity target, float amount) {
-        if (target instanceof Player) ((Player)entity).giveExperiencePoints((int) (10 * amount * amount));
+    public void tick(LivingEntity entity) {
+        CompoundTag tag = Affix.getAffixStorage(entity);
+        if (tag == null) return;
+        if (!tag.contains("barrier")) {
+            tag.putFloat("barrier", entity.getMaxHealth());
+        }
+        if (tag.contains("hurtTime") && tag.getInt("hurtTime") > 0) tag.putInt("hurtTime", tag.getInt("hurtTime") - 1);
+        else {
+            float shields = tag.getFloat("shields");
+            if (shields < entity.getMaxHealth() * 0.5f) {
+                tag.putFloat("shields", Math.min(shields + 1, entity.getMaxHealth() * 0.5f));
+                entity.heal(1);
+            }
+        }
     }
     
     @Override
